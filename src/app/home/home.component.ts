b8ef4c1f6a58c0e8abfac2582814f485dd5d030a
Java Student PC-7 <saimasonya22@gmail.com>
2025-03-04 12:01:20 +0600

    update

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Student } from '../model/student.model';
 
 @Component({
   selector: 'app-home',
@@ -7,5 +8,33 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  // student: Student [] = [];
+  // newStudent: Student = new Student('', '', '', '', '', '', '', '');
+  // isEditing: boolean = false;
+  // modal: bootstrap.Modal;
+  // constructor() {
+    
+  // }
+  // ngOnInit(): void {
+  //   throw new Error('Method not implemented.');
+  // }
 
-}
+  // openModal(student?: Student) {
+  //   if (student) {
+  //     this.newStudent = { ...student };
+  //     this.isEditing = true;
+  //   } else {
+  //     this.newStudent = new Student('', '', '', '', '', '', '', '');
+  //     this.isEditing = false;
+  //   }
+
+  //   const modalElement = document.getElementById('studentModal');
+  //   if (modalElement) {
+  //     this.modal = new bootstrap.Modal(modalElement);
+  //     this.modal.show();
+  //   }
+  // }
+   
+  }
+
+
