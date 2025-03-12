29c12218568cb330d77bdcd5aec582e66d7ab648
Java Student PC-7 <saimasonya22@gmail.com>
2025-03-12 13:25:23 +0600

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,16 +6,13 @@ import { AddCourseComponent } from './add-course/add-course.component';
 import { AdminPanelComponent } from './admin-panel/admin-panel.component';
 import { StudentListComponent } from './student-list/student-list.component';
 import { RegistrationComponent } from './registration/registration.component';
-<<<<<<< HEAD
 
 import { BookstoreComponent } from './bookstore/bookstore.component';
 import { DetailsComponent } from './details/details.component';
 import { ProgramdetailsComponent } from './programdetails/programdetails.component';
-=======
 import { RegisterComponent } from './register/register.component';
 import { AddbookComponent } from './addbook/addbook.component';
 import { BooklistComponent } from './booklist/booklist.component';
->>>>>>> 57ff4e749f7ac93b451c4758403816a15aee5bcb
 
 export const routes: Routes = [
     {path: '',component:HomeComponent},
@@ -26,12 +23,9 @@ export const routes: Routes = [
     {path:'admin', component:AdminPanelComponent},
     {path:'studentlist', component:StudentListComponent},
     {path:'register',component:RegistrationComponent},
-<<<<<<< HEAD
     {path:'bookstore',component:BookstoreComponent},
     {path:'details',component:DetailsComponent},
-    {path: 'coursedetails', component:ProgramdetailsComponent}
-=======
+    {path: 'coursedetails', component:ProgramdetailsComponent},
     {path:'addbook',component:AddbookComponent},
     {path:'booklist', component:BooklistComponent},
->>>>>>> 57ff4e749f7ac93b451c4758403816a15aee5bcb
 ];
